ff71cb0087d1ae49ea02d07cc249c6c06d05cf01
Thiraphat <thiraboaty@gmail.com>
2021-11-23 15:21:48 +0700

    tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import EzReactTable from "ez-react-table";
-import useFetch from "./useFetch";
+import EzReactTable from "ez-react-table"; //import eztable เพื่อที่จะนำค่าที่ได้จาก Url ทำเป็นตาราง
+import useFetch from "./components/useFetch"; //import method useFetch
 function App() {
+  //**
+  ตามที่ชอบเลยครับ;
+  // สร้างสไดล์ css
   const makeStyles = {
     fontFamily: "cursive ",
     fontSize: "15px",
@@ -15,14 +18,15 @@ function App() {
     height: "100vh",
   };
 
-  const getsData = useFetch("https://jsonplaceholder.typicode.com/users");
+  const getsData = useFetch("https://jsonplaceholder.typicode.com/users"); //เรียกใช่ useFetch โดยใส่ link  url ที่ต้องการ
 
+  // สร้าง columns ตามที่ต้องการโดยเลือกตามตัวเเปร จาก link ที่เรามีการใส่ไปใน useFetch()
   const columns = [
     {
       title: "Name",
-      key: "name",
+      key: "name", //อิงตามตัวเเปรที่อยู่ใน linkที่เราใส่
       width: 200,
-      center: true,
+      center: true, //เซ็ต column นี้ให้อยู่ตรงกลาง
 
       render: (str) => {
         return <span style={{ marginLeft: "10px" }}>{str}</span>;
@@ -30,40 +34,41 @@ function App() {
     },
     {
       title: "Username",
-      key: "username",
+      key: "username", //อิงตามตัวเเปรที่อยู่ใน linkที่เราใส่
       width: 200,
-      center: true,
+      center: true, //เซ็ต column นี้ให้อยู่ตรงกลาง
       render: (str) => {
-        return <>{str.toUpperCase()}</>;
+        return <>{str.toUpperCase()}</>; //compare to uppercase
       },
     },
     {
       title: "Email",
-      key: "email",
-      center: true,
-
+      key: "email", //อิงตามตัวเเปรที่อยู่ใน linkที่เราใส่
+      center: true, //เซ็ต column นี้ให้อยู่ตรงกลาง
       width: 200,
       render: (email) => <div>{email}</div>,
     },
     {
       title: "Website",
-      key: "website",
+      key: "website", //อิงตามตัวเเปรที่อยู่ใน linkที่เราใส่
       width: 200,
-      center: true,
+      center: true, //เซ็ต column นี้ให้อยู่ตรงกลาง
 
       render: (web) => <div>{web}</div>,
     },
   ];
 
   return (
-    <div style={makeStyles}>
+    // เรียกใช้ makeStyles ผ่าน style ใน div 
+    <div style={makeStyles}> 
+    {/* เรียกใช้ EzReactTable เเล้วใส่ path ของข้อมลเเต่ละตัว */}
       <EzReactTable
-        cols={columns}
-        data={getsData}
-        title="JSON TRACKER"
-        defaultSort="username"
-        tableHeight={400}
-        darkMode
+        cols={columns} // column
+        data={getsData} //data ที่ได้มาจาก  useFetch() โดยเก็บข้อมูลไว้ในตัวเเปร Data
+        title="JSON TRACKER" // หัวเรื่องของตา่ราง
+        defaultSort="username" // เรียงลำดับ สามารถเปลี่ยนได้ตามต้องการโดยดูจากใน Array object columns 
+        tableHeight={400} //ความสูงของตาราง
+        darkMode // apply darkmode or normal 
       />
     </div>
   );
--- src/components/useFetch.jsx
+++ src/components/useFetch.jsx
@@ -1,24 +1,28 @@
 // CustomHook API
 import { useState, useEffect } from "react";
 
-const useFetch = (url) => {
-  const [data, setData] = useState([]);
 
-  const fetched = async () => {
+//customHook มี รับ url เป้น URL
+const useFetch = (url) => { 
+  const [data, setData] = useState([]); // สร้างตัวเเปร data เเละ function setData เก็บเป็น Array
+
+  const fetched = async () => {//สร้าง async function fetched
+	//check code with try catch
     try {
-      const req = await fetch(url);
-      const res = await req.json();
-      setData(res);
+      const req = await fetch(url); //fetch url
+      const res = await req.json(); //เเปลง json เป็น javascript object โดยผ่าน json()
+      setData(res); //เรียกใช้function setData เเล้วส่งdataเป็น อาร์กิวเมนต์ เพื่อนำไปเเสดงผ่าน ตัวเเปร data
     } catch (error) {
-	    console.error(error.message)
+	    console.error(error.message) // catch error
     }
   };
 
+  //เรียกใช้ useEffect โดยรับ props url เพื่อที่เวลามีการเปลี่ยนเเปลงค่าใน url จะทำให้ function fetched มีการเปลี่ยนค่าตามไปด้วย
   useEffect(() => {
-    fetched();
-  }, [url]);
+    fetched(); //เรียกใช้ function fetched
+  }, [url]);// props is url
 
-  return data;
+  return data; //ส่งค่า data ที่ได้มาทั้้งหมดไปยัง useFetch เพื่อใช้เป็น method
 };
 
-export default useFetch;
+export default useFetch; 
